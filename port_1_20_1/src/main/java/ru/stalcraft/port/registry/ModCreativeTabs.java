401cc02673fc612dc64a6eff426d4a8f6d72f28a
Supportstarcoins <adamovreich@gmail.com>
2026-02-25 18:52:35 +0300

    Fix Forge 1.20.1 registry and sound constant compatibility

--- port_1_20_1/src/main/java/ru/stalcraft/port/registry/ModCreativeTabs.java
+++ port_1_20_1/src/main/java/ru/stalcraft/port/registry/ModCreativeTabs.java
@@ -1,10 +1,10 @@
 package ru.stalcraft.port.registry;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.Registries;
 import net.minecraftforge.registries.RegistryObject;
 import ru.stalcraft.port.StalkerPortMod;
 
@@ -14,7 +14,7 @@ public final class ModCreativeTabs {
     public static final RegistryObject<CreativeModeTab> STALKER_PORT_TAB = CREATIVE_MODE_TABS.register("stalker_port", () -> CreativeModeTab.builder()
         .title(Component.translatable("itemGroup.stalker_port"))
         .icon(() -> ModItems.VODKA.get().getDefaultInstance())
-        .displayItems((parameters, output) -> ModItems.getRegisteredItems().forEach(output::accept))
+        .displayItems((parameters, output) -> ModItems.getRegisteredItems().forEach(item -> output.accept(item.get())))
         .build());
 
     private ModCreativeTabs() {
