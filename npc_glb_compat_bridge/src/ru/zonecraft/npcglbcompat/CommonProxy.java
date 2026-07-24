b80daf3eb65bb831cc8cb891419de9616a2cc054
Supportstarcoins <adamovreich@gmail.com>
2026-07-25 01:22:32 +0300

    Register Stalker projectile damage fallback

--- npc_glb_compat_bridge/src/ru/zonecraft/npcglbcompat/CommonProxy.java
+++ npc_glb_compat_bridge/src/ru/zonecraft/npcglbcompat/CommonProxy.java
@@ -4,6 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
     public void registerHandlers() {
+        MinecraftForge.EVENT_BUS.register(new NpcStalkerProjectileFallbackEvents());
         MinecraftForge.EVENT_BUS.register(new NpcVortexCompatEvents());
     }
 }
