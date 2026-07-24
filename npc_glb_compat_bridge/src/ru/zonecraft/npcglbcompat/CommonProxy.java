315d302d7835fa310107043e1da65ae2e4a36cea
Supportstarcoins <adamovreich@gmail.com>
2026-07-25 01:02:05 +0300

    Register NPC weapon damage and vortex handlers

--- npc_glb_compat_bridge/src/ru/zonecraft/npcglbcompat/CommonProxy.java
+++ npc_glb_compat_bridge/src/ru/zonecraft/npcglbcompat/CommonProxy.java
@@ -4,6 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 
 public class CommonProxy {
     public void registerHandlers() {
+        MinecraftForge.EVENT_BUS.register(new NpcStalkerWeaponDamageEvents());
         MinecraftForge.EVENT_BUS.register(new NpcVortexCompatEvents());
     }
 }
