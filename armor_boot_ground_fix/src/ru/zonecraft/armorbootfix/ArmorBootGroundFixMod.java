386e4f12baf24316020768c5008ea8abb6032c6e
Supportstarcoins <adamovreich@gmail.com>
2026-07-25 13:31:58 +0300

    Bump universal boot fix to 1.1.0

--- armor_boot_ground_fix/src/ru/zonecraft/armorbootfix/ArmorBootGroundFixMod.java
+++ armor_boot_ground_fix/src/ru/zonecraft/armorbootfix/ArmorBootGroundFixMod.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 )
 public final class ArmorBootGroundFixMod {
     public static final String MODID = "zonecraftarmorbootfix";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @SidedProxy(
         clientSide = "ru.zonecraft.armorbootfix.ClientProxy",
@@ -30,7 +30,7 @@ public final class ArmorBootGroundFixMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         proxy.registerHandlers();
-        System.out.println("[Zonecraft Armor Boot Fix] 1.0.0 initialized; lift="
+        System.out.println("[Zonecraft Armor Boot Fix] 1.1.0 initialized; lift="
                 + BootFixSettings.baseLiftBlocks + " blocks.");
     }
 }
