c8570e984c6fb323b890e4ad4ad9cd9110d68c5c
Supportstarcoins <adamovreich@gmail.com>
2026-07-25 12:52:37 +0300

    Add common proxy for armor boot fix

---
+++ armor_boot_ground_fix/src/ru/zonecraft/armorbootfix/CommonProxy.java
@@ -0,0 +1,7 @@
+package ru.zonecraft.armorbootfix;
+
+public class CommonProxy {
+    public void registerHandlers() {
+        // Client-only visual correction. Dedicated servers load this class safely.
+    }
+}
